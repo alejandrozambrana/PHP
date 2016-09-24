b65e90d6be4ba019a4e976fd11f45cb97f61b204
alejandrozambrana <ale_1996_zn@hotmail.com>
2016-09-24 16:49:42 +0200

    prueba github en windows

---
+++ Ejercicios/02_RecogidaDeDatosPorTeclado/Ejercicio3/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
