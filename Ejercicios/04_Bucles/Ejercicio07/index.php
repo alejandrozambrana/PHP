55c978391cc19503781c8d24db2207abe3b41737
Alejandro Zambrana Naranjo <ale_1996_zn@hotmail.com>
2016-10-06 09:36:52 +0200

    PHP

--- Ejercicios/04_Bucles/Ejercicio07/index.php
+++ Ejercicios/04_Bucles/Ejercicio07/index.php
@@ -42,23 +42,19 @@ satisfactoriamente”. Tendremos cuatro oportunidades para abrir la caja fuerte.
     $numero = $_GET['anadir'];
     $combinacion = $_GET['combinacion'];
     $anadido = $numero;
-    $claveSeguridad = 1234;
-    $combinacion = ($combinacion * 10) + $anadido;        
+    $claveSeguridad = "1234";
+    $combinacion = $combinacion . $anadido;        
     $numeroDigitos = $combinacion; 
-    $contador = 0;
     
-    do{
-      $numeroDigitos = floor($numeroDigitos/10);
+    $numeroDigitos = strlen($combinacion);
       
-      $contador++;
-    }while($numeroDigitos >0);
     
-    if(($combinacion == $claveSeguridad) && ($contador == 4)){
+    if(($combinacion === $claveSeguridad) && ($numeroDigitos == 4)){
       echo "<div id=\"acceso\"><h1>ACCESO PERMITIDO</h1></div>";
-      $combinacion = 0;
-    } else if(($combinacion != $claveSeguridad) && ($contador == 4)){
+      $combinacion = "";
+    } else if($numeroDigitos == 4){
       echo "<div id=\"acceso\"><h1>ACCESO DENEGADO</h1></div>";
-      $combinacion = 0;
+      $combinacion = "";
     }
     
     ?>
@@ -68,16 +64,16 @@ satisfactoriamente”. Tendremos cuatro oportunidades para abrir la caja fuerte.
     <div id="cajaFuerte">
       <img src="caja fuerte.jpg" id="cajaFuerte" alt="cajaFuerte" title="cajaFuerte" usemap="#botonesMap">
       <map name="botonesMap">
-	<area shape="rect" coords="115,250,241,165" alt="cajaFuerte" href="index.php?anadir=1&&combinacion=<?=$combinacion?>">
-        <area shape="rect" coords="255,250,381,165" alt="cajaFuerte" href="index.php?anadir=2&&combinacion=<?=$combinacion?>">
-        <area shape="rect" coords="390,250,517,165" alt="cajaFuerte" href="index.php?anadir=3&&combinacion=<?=$combinacion?>">
-        <area shape="rect" coords="115,335,241,255" alt="cajaFuerte" href="index.php?anadir=4&&combinacion=<?=$combinacion?>">
-        <area shape="rect" coords="255,335,381,255" alt="cajaFuerte" href="index.php?anadir=5&&combinacion=<?=$combinacion?>">
-        <area shape="rect" coords="390,335,517,255" alt="cajaFuerte" href="index.php?anadir=6&&combinacion=<?=$combinacion?>">
-        <area shape="rect" coords="115,425,241,345" alt="cajaFuerte" href="index.php?anadir=7&&combinacion=<?=$combinacion?>">
-        <area shape="rect" coords="255,425,381,345" alt="cajaFuerte" href="index.php?anadir=8&&combinacion=<?=$combinacion?>">
-        <area shape="rect" coords="390,425,517,345" alt="cajaFuerte" href="index.php?anadir=9&&combinacion=<?=$combinacion?>">
-        <area shape="rect" coords="255,515,381,435" alt="cajaFuerte" href="index.php?anadir=0&&combinacion=<?=$combinacion?>">
+	<area shape="rect" coords="115,250,241,165" alt="cajaFuerte" href="index.php?anadir=1&&combinacion=<?=$combinacion ?>">
+        <area shape="rect" coords="255,250,381,165" alt="cajaFuerte" href="index.php?anadir=2&&combinacion=<?=$combinacion ?>">
+        <area shape="rect" coords="390,250,517,165" alt="cajaFuerte" href="index.php?anadir=3&&combinacion=<?=$combinacion ?>">
+        <area shape="rect" coords="115,335,241,255" alt="cajaFuerte" href="index.php?anadir=4&&combinacion=<?=$combinacion ?>">
+        <area shape="rect" coords="255,335,381,255" alt="cajaFuerte" href="index.php?anadir=5&&combinacion=<?=$combinacion ?>">
+        <area shape="rect" coords="390,335,517,255" alt="cajaFuerte" href="index.php?anadir=6&&combinacion=<?=$combinacion ?>">
+        <area shape="rect" coords="115,425,241,345" alt="cajaFuerte" href="index.php?anadir=7&&combinacion=<?=$combinacion ?>">
+        <area shape="rect" coords="255,425,381,345" alt="cajaFuerte" href="index.php?anadir=8&&combinacion=<?=$combinacion ?>">
+        <area shape="rect" coords="390,425,517,345" alt="cajaFuerte" href="index.php?anadir=9&&combinacion=<?=$combinacion ?>">
+        <area shape="rect" coords="255,515,381,435" alt="cajaFuerte" href="index.php?anadir=0&&combinacion=<?=$combinacion ?>">
       </map>
       
     </div> 
